91e1f6e5067f6c524b10f4dd118da4e16b4aab3d
sakurayang <1304274443@qq.com>
2019-12-17 11:18:53 +0800

    vue init

--- config/config.example.js
+++
@@ -1,7 +0,0 @@
-export default {
-  //我觉得可以不用注释
-  web: {
-    host: "0.0.0.0",
-    port: 8080
-  }
-};
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,45 @@
+const pages = { //mulit pages conf
+  index: {
+    entry: 'src/index.js',
+    template: 'public/index.html',
+    filename: 'index.html',
+    title: '~主页⭐魔法使之家~'
+  },
+  game: {
+    entry: 'src/game.js',
+    template: 'public/index.html',
+    filename: 'game/index.html',
+    title: '~游戏⭐魔法使之家~'
+  },
+  light: {
+    entry: 'src/light.js',
+    template: 'public/index.html',
+    filename: 'light/index.html',
+    title: '~小说⭐魔法使之家~'
+  },
+  user: {
+    entry: 'src/user.js',
+    template: 'public/index.html',
+    filename: 'user/index.html',
+    title: '~用户中心⭐魔法使之家~'
+  }
+}
+
+const vue_conf = {
+  publicPath: './',
+  outputDir: 'dist',
+  assetsDir: 'public', //default: static
+  productionSourceMap: false,  //default: true
+  crossorigin: 'use-credentials', //default: undefined
+  pwa: {
+    name: "魔法使之家",
+    short_name: 'Ero',
+    themeColor: "pink"
+  }
+}
+
+
 module.exports = {
-  assetsDir: '@/public',
-  outputDir: '@/dist'
+  ...vue_conf,
+  pages
 }
